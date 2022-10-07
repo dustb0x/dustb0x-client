8b3d26ae3f4aff66d0556a66b2e4bd0f82f14f78
kmasuda1986 <kentarou_masuda@hacklab-tech.io>
2022-10-08 08:14:38 +0900

    fix: Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,17 +5,21 @@ import { appName } from '@/config/constants/app'
 
 // MUI
 import AppBar from '@mui/material/AppBar'
+import Avatar from '@mui/material/Avatar'
 import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
+import ListItemAvatar from '@mui/material/ListItemAvatar'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemIcon from '@mui/material/ListItemIcon'
 import ListItemText from '@mui/material/ListItemText'
 import IconButton from '@mui/material/IconButton'
 import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
+import Modal from '@mui/material/Modal'
 import Stack from '@mui/material/Stack'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
@@ -25,10 +29,27 @@ import DashboardIcon from '@mui/icons-material/Dashboard';
 import MenuIcon from '@mui/icons-material/Menu'
 import MoreIcon from '@mui/icons-material/MoreVert'
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
+const pointer = {
+  cursor: 'pointer'
+}
+
 const Navbar: React.FC = () => {
   const router = useRouter()
   const [drawer, setDrawer] = useState<boolean>(false)
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const [walletConnectModal, setWalletConnectModal] = useState<boolean>(false)
 
   const open = Boolean(anchorEl)
 
@@ -62,7 +83,7 @@ const Navbar: React.FC = () => {
               display: { xs: 'none', sm: 'block' }
             }}
           >
-            <Button color="primary" variant="contained">Connect Wallet</Button>
+            <Button color="primary" variant="contained" onClick={() => setWalletConnectModal(true)}>Connect Wallet</Button>
           </Stack>
 
           <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
@@ -86,7 +107,7 @@ const Navbar: React.FC = () => {
         open={open}
         onClose={() => setAnchorEl(null)}
       >
-        <MenuItem>Connect Wallet</MenuItem>
+        <MenuItem onClick={() => setWalletConnectModal(true)}>Connect Wallet</MenuItem>
       </Menu>
 
       <Drawer
@@ -95,7 +116,10 @@ const Navbar: React.FC = () => {
       >
         <Box sx={{ width: 250 }}>
           <List>
-            <ListItem key="dashboard" disablePadding>
+            <ListItem
+              key="dashboard"
+              disablePadding
+            >
               <ListItemButton>
                 <ListItemIcon>
                   <DashboardIcon />
@@ -106,6 +130,40 @@ const Navbar: React.FC = () => {
           </List>
         </Box>
       </Drawer>
+
+      <Modal
+        open={walletConnectModal}
+        onClose={() => setWalletConnectModal(false)}
+        aria-labelledby="modal-modal-title"
+      >
+        <Box sx={style}>
+          <Typography
+            id="modal-modal-title"
+            variant="h6"
+          >
+            Connect wallet
+          </Typography>
+
+          <List>
+            <ListItem
+              sx={pointer}
+              onClick={() => console.log('テスト')}
+            >
+              <ListItemText>
+                Metamask
+              </ListItemText>
+              <ListItemAvatar>
+                <Avatar alt="Metamask" src="/metamask.svg" />
+              </ListItemAvatar>
+            </ListItem>
+            <ListItem>
+              <ListItemText>
+              Other wallets coming soon...
+              </ListItemText>
+            </ListItem>
+          </List>
+        </Box>
+      </Modal>
     </>
   )
 }
